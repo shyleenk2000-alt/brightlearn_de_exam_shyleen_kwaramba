307627fa2e8cb9f8a9395c1c70b7e8e3b9da9e1c
shyleen <shyleenk2000@gmail.com>
2026-07-18 00:07:18 +0200

    added cleaned database

--- creating_databases/database_script.sql
+++ creating_databases/database_script.sql
@@ -3,6 +3,11 @@ BEGIN
     CREATE DATABASE stg_brightlearn_sales;
 END;
 
+IF DB_ID('cleaned_brightlearn_sales') IS NULL
+BEGIN
+    CREATE DATABASE cleaned_brightlearn_sales;
+END; 
+
 IF DB_ID('dwh_brightlearn_sales') IS NULL
 BEGIN
     CREATE DATABASE dwh_brightlearn_sales;
